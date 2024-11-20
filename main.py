3dadbe162b49ae56edf24cfa12a34fbeb05c995a
Aless <alessandro.galiano1702@gmail.com>
2024-11-20 18:00:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import numpy as np
 import time as t
 
+def sum (t1, t2):
+    n = np.size(t1)
+    t3 = np.zero(n)
+    
+
+
 def strassen (t1, t2):
     n = np.size(t1)
     c = np.zero(n)
     assert (np.size(t2) != n)
-    a1 = np.zero(n//2)
-    a2 = np.zero(n//2)
-    a3 = np.zero(n//2)
-    a4 = np.zero(n//2)
-    b1 = np.zero(n//2)
-    b2 = np.zero(n//2)
-    b3 = np.zero(n//2)
-    b4 = np.zero(n//2)
-    m1 = np.zero(n//2)
-    m2 = np.zero(n//2)
-    m3 = np.zero(n//2)
-    m4 = np.zero(n//2)
-    m5 = np.zero(n//2)
-    m6 = np.zero(n//2)
-    m7 = np.zero(n//2)
+    a1 = t1.reshape()
+    a2 = t1.reshape()
+    a3 = t1.reshape()
+    a4 = t1.reshape()
+    b1 = t2.reshape()
+    b2 = t2.reshape()
+    b3 = t2.reshape()
+    b4 = t2.reshape()
+    m1 = (a1 + a4) * (b1 + b4)
+    m2 = 
+    m3 = 
+    m4 = 
+    m5 = 
+    m6 = 
+    m7 = 
     for i in range(n//2):
-        a1
+        for j in range(n//2):
+            a1 []
