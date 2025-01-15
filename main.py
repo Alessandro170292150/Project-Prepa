1fdab4ef5c84fb46d62a85a85d54b11437c2c044
Aless <alessandro.galiano1702@gmail.com>
2025-01-15 21:25:07 +0100

    ajout mlt dans python

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import numpy as np
 import time as t
 import random as rand
-
+import matplotlib.pyplot as plt
 
 def strassen (t1, t2):
-    if np.size(t1) == 1:
+    n = t1.shape[0]
+    if n == 1:
         return t1 * t2 
-    c = np.zero(n)
-    a1 = t1.reshape()
-    a2 = t1.reshape()
-    a3 = t1.reshape()
-    a4 = t1.reshape()
-    b1 = t2.reshape()
-    b2 = t2.reshape()
-    b3 = t2.reshape()
-    b4 = t2.reshape()
-    a1[:, :] = t1[:n//2, :n//2]
-    a2[:, :] = t1[(n//2):, :n//2]
-    a3[:, :] = t1[:(n//2), (n//2):]
-    a4[:, :] = t1[(n//2):, (n//2):]
-    b1[:, :] = t2[:n//2, :n//2]
-    b2[:, :] = t2[(n//2):, :n//2]
-    b3[:, :] = t2[:(n//2), (n//2):]
-    b4[:, :] = t2[(n//2):, (n//2):]    
+    c = np.zeros_like(t1)
+    a1, a2 = t1[:n//2, :n//2], t1[:n//2, n//2:]
+    a3, a4 = t1[n//2:, :n//2], t1[n//2:, n//2:]
+    b1, b2 = t2[:n//2, :n//2], t2[:n//2, n//2:]
+    b3, b4 = t2[n//2:, :n//2], t2[n//2:, n//2:]
     m1 = strassen(a1 + a4, b1 + b4)
     m2 = strassen(a2 + a4, b1)
     m3 = strassen(a1, b3 - b4)
@@ -36,6 +25,28 @@ def strassen (t1, t2):
     c[(n//2):, (n//2):] = m1 + m3 + m6 - m2
     return c
 
-def main ():
-    m1 = np.fromfunction(rand.randint, (5, 5), dtype = int)
-    m2 = np.fromfunction(rand.randint, (5, 5), dtype = int)
+def repartition (n, vmax):
+    x = np.linspace(0, n, num = n)
+    y = np.zeros_like(x)
+    for i in range(n):
+        m = np.random.randint(-abs(vmax), abs(vmax), size = (2**int(x[i]), 2**int(x[i])))
+        p = np.random.randint(-abs(vmax), abs(vmax), size = (2**int(x[i]), 2**int(x[i])))
+        t3 = t.time()
+        m2 = np.dot(m, p)
+        t4 = t.time()
+        dt2 = t4 - t3
+        y[i] = dt2
+        print(y[i])
+    a, b = np.polyfit(x, y, deg = 1)
+    y_est = a * x + b
+    y_err = x.std() * np.sqrt(1/len(x) + (x - x.mean())**2 / np.sum((x - x.mean())**2))
+    fig, ax = plt.subplots()
+    ax.plot(x, y_est, '-')
+    ax.fill_between(x, y_est - y_err, y_est + y_err, alpha=0.2)
+    ax.plot(x, y, 'o', color='tab:brown')
+    plt.show()
+
+
+
+repartition(12, 2*10)
+
